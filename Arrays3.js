dfbbf6e75015ba5ce2284ff3bc2e21eb36fce3d0
RedietZeraiHadera <redietzeraihadderra@gmail.com>
2023-04-07 11:44:55 +0300

    commit

--- Arrays3.js
+++ Arrays3.js
@@ -4,7 +4,7 @@
 //1.  Write a function that accepts an array of strings and 
 // console.logs each element using a for loop.
 
-let arr = ["Ann","Aketch","Atong"];
+let arr = ["Banana","Lemon","Orange"];
 string1(arr)
 function string1(arr){
 for (let i =0;i<arr.length;i++)
@@ -13,7 +13,7 @@ console.log(arr[i]);
 
 // 2. Write a function that accepts an array of numbers 
 // and uses the forEach() method to console.log each number multiplied by 2.
-let num= [22,54,34,67,23];
+let num= [2,4,3,7,13];
 arr3(num);
 
 function arr3(num){
@@ -24,7 +24,7 @@ num.forEach(numb=> {
 
 //3. Write a function that takes in an array of numbers and consoles the first four items
 //  multiplied by 8 and the last two added by 5. Console the array with the new values
-let arr5=[89,32,54,24,65,85];
+let arr5=[12,24,34,54,15,5];
 arr4(arr5)
 
 function arr4(arr5){
@@ -39,13 +39,13 @@ console.log(arr5);
 //4. Write a function that takes in the following array and use a while loop 
 // to iterate and break when the item is equal to the fourth index
 
-let arr7 = [1,2,3,4,5,6,7,8,9];
-arr6(arr7)
+let arrN = [1,2,3,4,5,6,7,8,9];
+arrM(arrN)
 
-function arr6(arr7){
+function arrM(arrN){
 let i =0;
-while(i<arr7.length){
-    console.log(arr7[i]);
+while(i<arrN.length){
+    console.log(arrN[i]);
     if(i===4){
        
         break;
@@ -57,14 +57,14 @@ while(i<arr7.length){
 
 // 5. Write a function that takes in a an array of strings and use a continue 
 // statement when the item is at the second index
-let fruits= ['apple','plum','banana','strawberries','kiwi']
-skipIndex(fruits)
-function skipIndex(fruits) {
-  for (let i = 0; i < fruits.length; i++) {
+let names= ['Rediet','Zeray','Hadera','Kidane','Engda']
+skip(names)
+function skip(names) {
+  for (let i = 0; i < names.length; i++) {
     if (i === 2) {
       continue;
     }
-    console.log(fruits[i]);
+    console.log(names[i]);
   }
 }
 
